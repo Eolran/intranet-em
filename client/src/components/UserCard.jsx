eb05fe3572507403cec1a7eb74af04ceec10fe46
Eolran <nevadasmith147@gmail.com>
2022-09-13 17:11:38 +0100

    Create(Store & Reducer & ListUsers)

---
+++ client/src/components/UserCard.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom'
+import '../assets/css/App.css'
+import '../assets/css/Nav.css'
+
+function UserCard({ user }) {
+
+    return (
+        <div className="Card">
+            <Link to={"/"}>
+                <div className='CardInfo'>
+                    <div>
+                        <img src={user.photo} alt="avatar" />
+                    </div>
+                    <div className='UserInfo'>
+                        <span>
+                            <b>{user.firstname} {user.lastname}</b>
+                        </span>
+                        <span>
+                            {user.city}, {user.country}
+                        </span>
+                        <span>
+                            {user.email}
+                        </span>
+                        <span>
+                            {user.phone}
+                        </span>
+                    </div>
+                    <span className='tag'>
+                        {user.service}
+                    </span>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default UserCard
--- client/src/views/listUsers.jsx
+++ client/src/views/listUsers.jsx
@@ -1,10 +1,40 @@
+import { useSelector } from 'react-redux'
 import '../assets/css/App.css'
+import '../assets/css/List.css'
+import Nav from '../components/Nav'
+import { ShowList } from '../services/api.service.js';
+import UserCard from '../components/UserCard'
+
 
 function ListUsers() {
+    ShowList();
+
+    const user = useSelector((state) => 
+    state.user
+  )
 
     return (
         <div className="App">
-            Test Liste Users
+            <Nav />
+            <button onClick={ShowList}>List</button>
+            <div className='List'>
+                <div className='Search'>
+                    <input type="text" placeholder='Recherche' />
+                    Rechercher par : 
+                    <select name="noms" id="">
+                        <option value="">Tous</option>
+                    </select>
+                    <select name="categorie" id="">
+                        <option value="">Tous</option>
+                    </select>
+                </div>
+                <div className='UsersList'>
+                    { 
+                        user.map((user, i) => <UserCard user = {user} key={Date.now + i} />)
+                    }
+                </div>
+            </div>
+            
         </div>
     )
 }
