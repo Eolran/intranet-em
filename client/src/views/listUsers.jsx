48f047c6450df90f031baa76c63b5f07a13261eb
Eolran <nevadasmith147@gmail.com>
2022-09-16 11:47:56 +0100

    Start(search algo)

--- client/src/views/listUsers.jsx
+++ client/src/views/listUsers.jsx
@@ -1,31 +1,49 @@
+import { useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import '../assets/css/App.css'
 import '../assets/css/List.css'
 import UserCard from '../components/UserCard'
 import { ShowList } from '../services/api.service.js';
+import { Search } from '../services/search.service.js';
 
 
 const token = localStorage.getItem('token');
 {token && ShowList()}
 
+
 function ListUsers() {
+
+    const SearchValue = useRef("");
+    const [select1, setSelect1] = useState("name");
+    const [select2, setSelect2] = useState(null);
 
     const user = useSelector((state) => 
     state.user
   )
-  console.log(user);
+
+
+    function OnSearch(e) {
+        SearchValue.current = e.target.value;
+        Search(SearchValue, user, select1, select2)
+        // console.log(SearchValue);
+        console.log(select1, select2);
+    }
 
     return (
         <div className="App">
             <div className='List'>
                 <div className='Search'>
-                    <input type="text" placeholder='Recherche' />
+                    <input onChange={(e) => OnSearch(e)} type="text" id='inputSearch' placeholder='Recherche' />
                     Rechercher par : 
-                    <select name="noms" id="">
-                        <option value="">Tous</option>
+                    <select onChange={(e) => setSelect1(e.target.value)} name="select1" id="select1Search">
+                        <option value="name">Noms</option>
+                        <option value="place">Localisation</option>
                     </select>
-                    <select name="categorie" id="">
-                        <option value="">Tous</option>
+                    <select onChange={(e) => setSelect2(e.target.value)} name="select2" id="select2Search">
+                        <option value="all">Tous</option>
+                        <option value="Client">Client</option>
+                        <option value="Marketing">Marketing</option>
+                        <option value="Technique">Technique</option>
                     </select>
                 </div>
                 <div className='UsersList'>
