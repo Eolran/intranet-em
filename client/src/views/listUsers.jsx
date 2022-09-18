f2ca1e92d2854179d452fcbfcacaf22b57ed074f
Eolran <nevadasmith147@gmail.com>
2022-09-18 21:46:14 +0100

    Update(CSS)

--- client/src/views/listUsers.jsx
+++ client/src/views/listUsers.jsx
@@ -43,19 +43,21 @@ function ListUsers() {
     return (
         <div className="App">
             <div className='List'>
-                <div className='Search'>
-                    <input onInput={(e) => OnSearch(e)} type="text" id='inputSearch' placeholder='Recherche' />
-                    Rechercher par : 
-                    <select onChange={(e) => setSelect1(e.target.value)} name="select1" id="select1Search">
-                        <option value="name">Noms</option>
-                        <option value="place">Localisation</option>
-                    </select>
-                    <select onChange={(e) => setSelect2(e.target.value)} name="select2" id="select2Search">
-                        <option value="all">Tous</option>
-                        <option value="Client">Client</option>
-                        <option value="Marketing">Marketing</option>
-                        <option value="Technique">Technique</option>
-                    </select>
+                <div className='SearchOutline'>
+                    <div className='SearchInline'>
+                        <input onInput={(e) => OnSearch(e)} type="text" id='inputSearch' placeholder='Recherche' />
+                        Rechercher par : 
+                        <select onChange={(e) => setSelect1(e.target.value)} name="select1" id="select1Search">
+                            <option value="name">Noms</option>
+                            <option value="place">Localisation</option>
+                        </select>
+                        <select onChange={(e) => setSelect2(e.target.value)} name="select2" id="select2Search">
+                            <option value="all">Tous</option>
+                            <option value="Client">Client</option>
+                            <option value="Marketing">Marketing</option>
+                            <option value="Technique">Technique</option>
+                        </select>
+                    </div>
                 </div>
                 <div className='UsersList'>
                     { 
